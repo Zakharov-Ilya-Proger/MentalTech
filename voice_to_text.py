78721b23249a3260231be0842c935b02e588c0d9
Iiya Zakharov <2146376@gmail.com>
2025-01-27 14:56:35 +0300

    Add OGG and update prompt

--- voice_to_text.py
+++ voice_to_text.py
@@ -4,25 +4,7 @@ from io import BytesIO
 import soundfile as sf
 from vosk import KaldiRecognizer, Model
 
-
-async def transcribe_wav(file_content: bytes, model: Model) -> str:
-    wf = wave.open(BytesIO(file_content), "rb")
-    rec = KaldiRecognizer(model, wf.getframerate())
-    rec.SetWords(True)
-    text_from_file = ""
-    while True:
-        data = wf.readframes(4000)
-        if len(data) == 0:
-            break
-        if rec.AcceptWaveform(data):
-            result = rec.Result()
-            result_dict = json.loads(result)
-            text_from_file += result_dict.get("text", "") + " "
-    rec.FinalResult()
-    return text_from_file
-
-
-async def transcribe_mp3(file_content: bytes, model: Model) -> str:
+async def transcribe_ogg(file_content: bytes, model: Model) -> str:
     data, samplerate = sf.read(BytesIO(file_content))
     wav_buffer = BytesIO()
     sf.write(wav_buffer, data, samplerate, format='WAV')
