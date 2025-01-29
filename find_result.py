0abbf9e892261a56f353838f7ec8959cf7f84038
Iiya Zakharov <2146376@gmail.com>
2025-01-29 21:29:25 +0300

    Update model params temperature: 0.5, top_p: 0.65, top_k: 80

--- find_result.py
+++ find_result.py
@@ -2,8 +2,8 @@ import re
 
 
 def extract_results(response_text: str):
-    gad7_pattern = r"Результаты по шкале GAD-7:\s*(\d/\d/\d/\d/\d/\d/\d)"
-    phq9_pattern = r"Результаты по шкале PHQ-9:\s*(\d/\d/\d/\d/\d/\d/\d/\d/\d)"
+    gad7_pattern = r"GAD-7:\s*(\d/\d/\d/\d/\d/\d/\d)"
+    phq9_pattern = r"PHQ-9:\s*(\d/\d/\d/\d/\d/\d/\d/\d/\d)"
 
     gad7_match = re.search(gad7_pattern, response_text)
     gad7_result = gad7_match.group(1) if gad7_match else None
